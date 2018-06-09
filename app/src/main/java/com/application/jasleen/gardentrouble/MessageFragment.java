8e2734593a399caaffea735f69affc92177f0542
Jasleen <jka185@sfu.ca>
2018-06-09 16:38:53 -0700

    Implemented Congratulations Dialog
    Implemented with text and images

--- app/src/main/java/com/application/jasleen/gardentrouble/MessageFragment.java
+++ app/src/main/java/com/application/jasleen/gardentrouble/MessageFragment.java
@@ -4,11 +4,14 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 public class MessageFragment extends AppCompatDialogFragment {
     @Override
@@ -25,14 +28,22 @@ public class MessageFragment extends AppCompatDialogFragment {
             public void onClick(DialogInterface dialog, int which) {
                 Log.i("MessageFragment", "You have won the game" );
 
-                Intent intent = MenuActivity.makeMenuIntent(getActivity());
-                startActivity(intent);
+                // Closes Game Activity and goes back to the Main Menu Activity
+                getActivity().finish();
             }
         };
+        TextView title = new TextView(getActivity());
+// You Can Customise your Title here
+        title.setText(R.string.congratulations_message_title);
+        title.setBackgroundColor(Color.DKGRAY);
+        title.setPadding(10, 10, 10, 10);
+        title.setGravity(Gravity.CENTER);
+        title.setTextColor(Color.WHITE);
+        title.setTextSize(20);
 
         //Build the alert dialog
         return new AlertDialog.Builder(getActivity())
-                .setTitle("Congratulations! You Won!")
+                .setCustomTitle(title)
                 .setView(view)
                 .setPositiveButton(android.R.string.ok,listener)
                 .create();
